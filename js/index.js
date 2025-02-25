4dd2d00d4fd96bbf6caf0d5d2c81a04eb9ab371c
aonydes@gmail.com <aonydes@gmail.com>
2025-02-25 18:10:46 -0300

    Se agrega el proyecto

---
+++ js/index.js
@@ -0,0 +1,49 @@
+const producto = []
+const URL = "js/productos.json"
+
+const plato = document.querySelector('div.plato')
+
+retornarCardHtml = (producto) => {
+    return `<div class="morfi">
+                <div class="morfi-image">${producto.imagen}</div>
+                <div class="morfi-name">${producto.nombre}</div>
+                <div class="morfi-price">${producto.precio}</div>
+                <div class="morfi-button">
+                    <button class="button button-outline button-add" id="${producto.id}" title="Clic para agregar al carrito">+</button>
+                </div>
+            </div>`
+}
+
+const activarClickEnBotones = () =>{
+    const botonesAgregar = document.querySelectorAll('button.button-outline.button-add')
+    if(botonesAgregar !== null){
+        botonesAgregar.forEach((button)=>{
+            button.addEventListener('click', (e)=>{
+                agregarAlCarrito(e.target.id)
+            })
+            })
+        }
+
+    }
+
+
+const cargarProductos = (array) => {
+    if(array.length > 0){
+        array.forEach(producto => {
+            plato.innerHTML += retornarCardHtml(producto)
+        });
+        activarClickEnBotones()
+    }
+}
+
+const obtenerProductos = () => {
+    fetch(URL)
+    .then((response) => response.json())
+    .then((data) => producto.push(...data))
+
+    .then(() => cargarProductos(producto))
+}
+
+//cargarProductos(productos)
+
+obtenerProductos()
